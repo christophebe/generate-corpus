14b03b03e0b55910d4146fe50d73f61ccddb6211
christophebe <christophe.lombart@gmail.com>
2017-10-17 16:40:06 +0200

    first implementation for word2vec

--- test/test-w2v.js
+++ test/test-w2v.js
@@ -1,13 +1,48 @@
+const util = require("util");
+const mkdir = util.promisify(require("fs").mkdir);
 const should = require("chai").should();
 const corpus = require("../index.js");
 
+
+const simpleSearch = {
+  host: "google.be",
+  num: 20,
+  qs: {
+    q: ["prêt personnel"],
+    pws: 0,
+
+  },
+  language: "fr",
+
+};
+
 const outputDir = "./test/test-save-corpus";
 
-describe("Extract data from a corpus", () => {
-  it("test", function test() {
+describe("Save a genetated corpus", () => {
+  before((done) => {
+    mkdir(outputDir)
+      .catch(error => console.log("ignore"));
+
+    done();
+  });
+
+  it.only("test save a corpus of 10 documents from a Google SERP & generate vectors", function test() {
     this.timeout(1000000);
-    corpus.buildVectors(outputDir)
-      // .then(corpus => corpus.should.to.have.lengthOf(10))
-      .catch(error => /* error.should.not.be.null */ console.log(error));
+
+    /*
+    corpus.saveCorpus(simpleSearch, outputDir)
+      .then(() => corpus.buildVectors(outputDir))
+      .then(() => corpus.loadCorpusModel(outputDir))
+      .catch(error => error.should.has.to.be.null);
+    */
+
+    corpus.loadCorpusModel(outputDir)
+      .then((model) => {
+        const wordVecs = model.mostSimilar("prêt personnel");
+        console.log(wordVecs);
+        model.getNearestWords(model.getVector("prêt"), 10);
+        console.log(wordVecs);
+      })
+      .catch(error => console.log(error));
   });
 });
